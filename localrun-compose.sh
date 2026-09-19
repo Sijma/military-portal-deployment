22f2659d14ad0eb47f4f9b8b7cf861a40550881d
sijma <johnbalasis@yahoo.gr>
2026-09-20 02:15:42 +0300

    update compose to use override for dev and no env files

--- localrun-compose.sh
+++ localrun-compose.sh
@@ -1,1 +1,7 @@
-docker compose --env-file .env up -d --build
+#!/bin/sh
+set -eu
+
+docker compose --env-file .env \
+  -f docker-compose.yml \
+  -f docker-compose.override.yml \
+  up -d --build
--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,10 @@
+\getenv app_database POSTGRES_DB
+\getenv portal_user PORTAL_DB_USER
+\getenv portal_password PORTAL_DB_PASSWORD
+\getenv keycloak_database KEYCLOAK_DB_NAME
+\getenv keycloak_user KEYCLOAK_DB_USER
+\getenv keycloak_password KEYCLOAK_DB_PASSWORD
+
 CREATE TABLE IF NOT EXISTS applications (
     applicant_amka TEXT PRIMARY KEY,
     applicant_id TEXT NOT NULL,
@@ -17,3 +24,22 @@ CREATE TABLE IF NOT EXISTS applications (
         (application_type = 'service' AND service_division IS NOT NULL AND deferment_reason IS NULL)
     )
 );
+
+SELECT format('CREATE ROLE %I LOGIN PASSWORD %L', :'portal_user', :'portal_password')
+WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = :'portal_user')
+\gexec
+
+GRANT CONNECT ON DATABASE :"app_database" TO :"portal_user";
+GRANT USAGE ON SCHEMA public TO :"portal_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO :"portal_user";
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO :"portal_user";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO :"portal_user";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO :"portal_user";
+
+SELECT format('CREATE ROLE %I LOGIN PASSWORD %L', :'keycloak_user', :'keycloak_password')
+WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = :'keycloak_user')
+\gexec
+
+SELECT format('CREATE DATABASE %I OWNER %I', :'keycloak_database', :'keycloak_user')
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = :'keycloak_database')
+\gexec
